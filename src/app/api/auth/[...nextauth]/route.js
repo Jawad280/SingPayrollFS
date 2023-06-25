c1b9545843cbd53cb8c393dfed1bbb1f8cf6c7c6
Jafthab <jafthab@gmail.com>
2023-06-25 22:12:23 +0800

    Update route.js

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -11,7 +11,7 @@ const handler = NextAuth({
           console.log("authorize function invoked");
           try {
 
-            const res = await fetch(`http://localhost:3000/api/users/username/${credentials.username}`);
+            const res = await fetch(`/api/users/username/${credentials.username}`);
   
             const user = await res.json();
             if (user) {
